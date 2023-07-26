fd558712c80d348540ff118ff7feb70f699aa406
Nitesh Kumar <imnksid@gmail.com>
2023-07-26 11:36:32 +0530

    Written comments

--- DummyRNProj/src/screens/Screen2.tsx
+++ DummyRNProj/src/screens/Screen2.tsx
@@ -3,7 +3,7 @@ import React, {useEffect, useRef, useState} from 'react';
 
 const Screen2 = ({navigation}) => {
   const [counter, setCounter] = useState(0);
-  const counterRef = useRef(counter);
+  const counterRef = useRef(counter); //This reference is used to pause and resume the counter value later.
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -11,19 +11,22 @@ const Screen2 = ({navigation}) => {
         return; // Pause the counter if the screen is not focused
       }
 
-      setCounter(prevCounter => prevCounter + 1);
-      counterRef.current = counter;
+      setCounter(prevCounter => prevCounter + 1); //Updating the counter value when the screen is focused
+      counterRef.current = counter; //After updating the counter value, update the reference value as well.
+      // This is for correctly resuming the counter when the user comes back to Screen2 after navigating away.
     }, 1000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); //Unmounting the component to prevent memory leaks and stop
+    //the counter from incrementing when the component is no longer in use.
   }, [navigation, counter]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('blur', () => {
-      counterRef.current = 0;
+      counterRef.current = 0; //When the user navigates away from Screen2, the counterRef.current is reset to 0.
+      //This ensures that when the user returns to Screen2, the counter will start from 0.
     });
 
-    return unsubscribe;
+    return unsubscribe; //Removing the listener on unmounting.
   }, [navigation]);
 
   return (
